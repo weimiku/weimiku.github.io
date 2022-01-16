fd05bc4aea33a48294f425c552fa86c051e1d8d6
Weiwei <1570682285@qq.com>
2022-01-16 20:18:34 +0800

    设置默认值

--- birthday/js/index.js
+++ birthday/js/index.js
@@ -2,7 +2,7 @@ $('#login-button').click(function (event) {
 	var userName=document.getElementById("userName").value;
     var pwd=document.getElementById("pwd").value;
 		//修改密码请改此处
-    if(userName=="小口袋" &&  pwd=="xkd1215"){
+    if(userName=="小口袋" &&  pwd=="1215"){
 			event.preventDefault();
 			$('form').fadeOut(500);
 			$('.wrapper').addClass('form-success');
